49dd3d7ae6947faeeb44d810589e274ee253bb93
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-09 15:20:37 +0100

    img inserite dall'array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 
+// array di oggetti 
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];    
+
+
 // array delle src delle img
-const imagesCarousel = [
-    "img/01.webp",
-    "img/02.webp",
-    "img/03.webp",
-    "img/04.webp",
-    "img/05.webp"
-];
+// const imagesCarousel = [
+//     "img/01.webp",
+//     "img/02.webp",
+//     "img/03.webp",
+//     "img/04.webp",
+//     "img/05.webp"
+// ];
 
 const sliderDom = document.getElementById("slider");
 const prevDom = document.getElementById("prev");
@@ -16,14 +42,16 @@ const thumbsDom = document.getElementById("thumbs");
 let sliderContent = "";
 let thumbsContent = "";
 
-for (let i = 0; i < imagesCarousel.length; i++) {
+for (let i = 0; i < images.length; i++) {
+
+    let carouselImage = images[i];
     
     let divImage = `<div class="image">
-                        <img src="${imagesCarousel[i]}">
+                        <img src="${carouselImage.image}">
                     </div>`;
 
     let divThumbs = `<div class="thumbs-images brightness">
-                        <img class="thumbs-img" src="${imagesCarousel[i]}">
+                        <img class="thumbs-img" src="${carouselImage.image}">
                      </div>`;                
     
     sliderContent += divImage;
